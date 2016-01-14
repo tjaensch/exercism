55754d7618cfc554e38223166c5814eaeae474af
Thomas Jaensch <thomasjaensch@gmail.com>
2016-01-14 16:16:13 -0500

    Added triangle.go solution

---
+++ go/triangle/triangle.go
@@ -0,0 +1,29 @@
+package triangle
+
+import "math"
+
+type Kind float64
+
+const (
+	Equ Kind = iota // equilateral
+	Iso             // isosceles
+	Sca             // scalene
+	NaT             // not a triangle
+)
+
+func KindFromSides(a, b, c float64) Kind {
+	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) {
+		return NaT
+	} else if a < 0 || b < 0 || c < 0 {
+		return NaT
+	} else if a+b <= c || b+c <= a || a+c <= b {
+		return NaT
+	} else if a == b && a == c {
+		return Equ
+	} else if a == b || a == c || b == c {
+		return Iso
+	} else {
+		return Sca
+	}
+
+}
