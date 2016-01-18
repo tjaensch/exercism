82b86e0f6ce960fa25b62ff0b063a61fae1a6d6e
Thomas Jaensch <thomasjaensch@gmail.com>
2016-01-18 11:50:53 -0500

    Added food_chain.go solution

---
+++ go/food-chain/food_chain.go
@@ -0,0 +1,75 @@
+package foodchain
+
+import "strings"
+
+const TestVersion = 1
+
+var song = []string{``,
+
+	`I know an old lady who swallowed a fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a spider.
+It wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a bird.
+How absurd to swallow a bird!
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a cat.
+Imagine that, to swallow a cat!
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a dog.
+What a hog, to swallow a dog!
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a goat.
+Just opened her throat and swallowed a goat!
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a cow.
+I don't know how she swallowed a cow!
+She swallowed the cow to catch the goat.
+She swallowed the goat to catch the dog.
+She swallowed the dog to catch the cat.
+She swallowed the cat to catch the bird.
+She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
+She swallowed the spider to catch the fly.
+I don't know why she swallowed the fly. Perhaps she'll die.`,
+
+	`I know an old lady who swallowed a horse.
+She's dead, of course!`,
+}
+
+func Song() string {
+	return strings.Join(song[1:], "\n\n")
+}
+
+func Verse(n int) string {
+	return Verses(n)
+}
+
+func Verses(n ...int) string {
+	var ret []string
+	for i := range n {
+		ret = append(ret, song[n[i]])
+	}
+	return strings.Join(ret, "\n\n")
+}
--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -4,8 +4,8 @@ import "time"
 
 const TestVersion = 2
 
-func AddGigasecond(Birthday time.Time) time.Time {
-	return Birthday.Add(time.Duration(1e9) * time.Second)
+func AddGigasecond(birthday time.Time) time.Time {
+	return birthday.Add(time.Duration(1e9) * time.Second)
 }
 
 var Birthday = time.Date(2016, time.January, 13, 0, 0, 0, 0, time.UTC)
