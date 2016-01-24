0cf8700868bcb59cfb886176023d33356be9234e
Thomas Jaensch <thomasjaensch@gmail.com>
2016-01-24 12:27:34 -0500

    Added hamming.go solution

---
+++ go/hamming/hamming.go
@@ -0,0 +1,22 @@
+package hamming
+
+import (
+	"errors"
+)
+
+const TestVersion = 2
+
+func Distance(a, b string) (hammingDistance int, err error) {
+	hammingDistance = 0
+
+	if len(a) != len(b) {
+		return 0, errors.New("Sequences of unequal length.")
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			hammingDistance++
+		}
+	}
+	return hammingDistance, nil
+}
